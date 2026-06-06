d671357b5755324e49fd2df3ce7962214dfb82e4
acb20si <sim6@sheffield.ac.uk>
2026-06-06 22:14:52 +0100

    login fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :validatable
+       
   belongs_to :group, optional: true
 
-  enum role: {
-    user: "user",
-    manager: "manager",
-    admin: "admin"
+  enum :role, {
+    user: 0,
+    manager: 1,
+    admin: 2
   }
 end
--- db/migrate/20260606190857_add_role_and_group_to_users.rb
+++ db/migrate/20260606190857_add_role_and_group_to_users.rb
@@ -1,6 +1,6 @@
 class AddRoleAndGroupToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :role, :string, default: "user", null: false
+    add_column :users, :role, :integer, default: 0, null: false
     add_reference :users, :group, foreign_key: true
     add_column :users, :approved, :boolean, default: false, null: false
   end
