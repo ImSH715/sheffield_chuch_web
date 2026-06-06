616e69c74db3956cc780d78c9cbb4993c6ebcc3a
acb20si <sim6@sheffield.ac.uk>
2026-06-06 20:24:19 +0100

    adding admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+admin = User.find_or_create_by!(email: "admin@sheffieldchurch.org") do |user|
+  user.password = "Password1234!"
+  user.role = "admin"
+  user.approved = true
+end
